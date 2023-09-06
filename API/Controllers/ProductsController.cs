1bae14ddedaebff1a00ca667a027049668ca4073
Gong Moonphruk <gong.moonphruk@gmail.com>
2023-09-06 18:07:56 +0200

    Setup API

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class ProductsController : ControllerBase
+	{
+		private readonly StoreContext _context;
+
+		public ProductsController(StoreContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		[ProducesResponseType(200)]
+		[ProducesResponseType(500)]
+		public async Task<ActionResult<List<Product>>> GetProducts()
+		{
+			return await _context.Products.ToListAsync();
+		}
+			
+		[HttpGet("{id}")]
+		[ProducesResponseType(200)]
+		[ProducesResponseType(400)]
+		[ProducesResponseType(404)]
+		[ProducesResponseType(500)]
+		public async Task<ActionResult<Product>> GetProduct(int id)
+		{
+			return await _context.Products.FindAsync(id);
+		}
+	}
+}
